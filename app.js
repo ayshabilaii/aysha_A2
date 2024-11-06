396e5dc3711c505dc1a6c8edf18917ea46e9293f
ayshabilaii <aysha.bilai@ontariotechu.net>
2024-11-05 21:23:28 -0500

    config files

--- app.js
+++ app.js
@@ -1,53 +1,43 @@
-/* SPA --> single pag eapplication */
-/* client side */
-// IIFE --> immediately invoked function expression 
-
-(function() {
-    function start() {
-        console.log("App Started");
-    }
-
-    window.addEventListener("load", start);
-})();
-
-/* installed 3rd party package */
 let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
+let indexRouter = require('../routes/index');
+let usersRouter = require('../routes/users');
+let bookRouter = require('../routes/book')
 
 let app = express();
-
+let mongoose = require('mongoose');
+let DB = require('./db');
+// point my mongoose to the URI
+mongoose.connect(DB.URI);
+let mongoDB = mongoose.connection;
+mongoDB.on('error',console.error.bind(console,'Connection Error'))
+mongoDB.once('open',()=>{
+  console.log('MongoDB Connected')
+})
+mongoose.connect(DB.URI,{useNewURIParser:true,
+  useUnifiedTopology:true
+})
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('../views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'node_modules')));
-
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../node_modules')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/books',bookRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
-//contact emails 
-app.post('/contact', (req, res) => {
-  const { name, email, comments, maillist } = req.body;
-  // Here, you can add logic to send an email or save the data
-  res.send('Thank you for contacting us!');
-});
-
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -57,11 +47,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error',{title:'Error'});
 });
 
-<form action="/contact" method="post" name="form"/>
-
-app.get('/contact', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'contact.ejs'));
-});
+module.exports = app;
